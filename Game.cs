7ab391c22b8ce80ab8a668e49812b49b5a4ff2dd
Oliver Willis <olliewillis57@gmail.com>
2024-12-21 13:34:45 +0000

    Improved Code

--- Game.cs
+++ Game.cs
@@ -14,7 +14,6 @@ namespace ChessEngine.Game
         public static bool WhiteKingCheck = false;
         public static bool BlackKingCheck = false;
         public static ulong[] Positions = new ulong[12];
-        public static List<Gamestate> Movelist = new List<Gamestate>();
 
         public void RunMainWindow()
         {
@@ -42,55 +41,8 @@ namespace ChessEngine.Game
 
         public static void IncrementTurn()
         {
-            IsCheckmate();
-            Console.WriteLine(IsCheckmate());
             SetPositions();
             turn++;
-        }
-
-        public static bool IsCheckmate()
-        {
-            WhiteKingCheck = IsAttacked(WhiteKing, true);
-            Console.WriteLine(WhiteKingCheck = IsAttacked(WhiteKing, true));
-            BlackKingCheck = IsAttacked(BlackKing, false);
-            Console.WriteLine(BlackKingCheck = IsAttacked(BlackKing, false));
-            if (WhiteKingCheck || BlackKingCheck)
-            {
-                ulong checkMask = WhiteKingCheck? GetCheckMask(WhiteKing, WhiteKingCheck) : GetCheckMask(BlackKing,WhiteKingCheck);
-                PrintBitBoard(checkMask,"checkMask");
-                Console.WriteLine(GetCheckPiece(checkMask,WhiteKingCheck));
-                Console.WriteLine(GetCheckPiece(checkMask,!WhiteKingCheck));
-                if (!IsAttacked(GetCheckPiece(checkMask,WhiteKingCheck),!WhiteKingCheck,false,true))
-                {
-                    if (!IsAttacked(checkMask, !WhiteKingCheck, false, false))
-                    {   
-                        if (IsAttacked(GenerateKingMoves(WhiteKingCheck ? WhiteKing : BlackKing, WhiteKingCheck, 0UL, false), WhiteKingCheck))
-                        {
-                            Console.WriteLine("");
-                            Console.WriteLine("Checkmate");
-                            Console.WriteLine($"Game ended in {turn} turns");
-                            string won = WhiteKingCheck ? "Black" : "White";
-                            Console.WriteLine($"{won} Won");
-                            return true;
-                        }
-                    }
-                }
-            }
-            return false;
-        }
-    }
-
-    public class Gamestate
-    {
-        public int TurnNum { get; private set; }
-        public ulong[] PiecesPositions { get; private set; }
-        public string MoveMade { get; private set; }
-
-        public Gamestate(int turnNum, ulong[] piecesPositions, string moveMade)
-        {
-            TurnNum = turnNum;
-            PiecesPositions = (ulong[])piecesPositions.Clone();
-            MoveMade = moveMade;
         }
     }
 }
